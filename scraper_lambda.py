fee306fb391abe00e6209ff9fbe3424421a5df81
yuzhouw313 <yuzhouw@uchicago.edu>
2023-05-19 11:14:46 -0500

    changed to scrape by subject and start index

--- scraper_lambda.py
+++ scraper_lambda.py
@@ -3,47 +3,43 @@ import dataset
 import json
 import requests
 
-api_key = 'AIzaSyBgPYASTWnKJfz_eNzjuywNHMUt6cZV2xY'
+api_key = 'AIzaSyBgPYASTWnKJfz_eNzjuywNHMUt6cZV2xY' # Google Books API key
 rds = boto3.client('rds')
-db_rds = rds.describe_db_instances()['DBInstances'][0] 
+db_rds = rds.describe_db_instances()['DBInstances'][0]
 ENDPOINT = db_rds['Endpoint']['Address']
 PORT = db_rds['Endpoint']['Port']
-db_url = 'mysql+mysqlconnector://{}:{}@{}:{}/books'.format(
-    'username',
-    'password',
-    ENDPOINT,
-    PORT)
+db_url = 'mysql+mysqlconnector://{}:{}@{}:{}/books'.format('username',
+                                                           'password',
+                                                           ENDPOINT,
+                                                           PORT)
 db = dataset.connect(db_url)
 
 
-def search_books(start_year, end_year):
-    ''' 
-    Scrape all books published in a given period of time
-    '''
-
+def search_books(subject, start_index):
     url = "https://www.googleapis.com/books/v1/volumes"
     parameters = {
         "q": "",
         "key": api_key,
         "printType": "books",
-        "startIndex": 0
+        "startIndex": start_index
     }
     books = []
+    parameters["q"] = f"categories:{subject}"
+    response = requests.get(url, params=parameters)
+    if response.status_code == 200:
+        results = json.loads(response.content)
+        books.extend(results["items"])
+    else:
+        print(f"Failed to fetch results for subject {subject} at star index {start_index}.")
 
-    for year in range(start_year, end_year+1):
-        parameters["q"] = f"pubdate:{year}"
-        response = requests.get(url, params=parameters)
-        if response.status_code == 200:
-            results = json.loads(response.content)
-            books.extend(results["items"])
-        else:
-            print(f"Failed to fetch results for year {year}.")
+    
     return books
 
 
 def store_books_in_db(books):
     ''' 
     Helper function to store all scraped books into db
+    Input (list): information of all books from search_books function
     '''
 
     table = db['book_info']
@@ -59,18 +55,20 @@ def store_books_in_db(books):
             'description': book['volumeInfo']['description'] if 'description' in book['volumeInfo'] else None,
             'categories': book['volumeInfo']['categories'] if 'categories' in book['volumeInfo'] else None,
         }
-        table.upsert(book_info, keys=['book_id'])
+        # book_id: unique identifier for a specific volume in Google Books API, like "9bnyDwAAQBAJ"
+        table.upsert({'book_id': book['id'], 'book_info': json.dumps(book_info)}, ['book_id']) 
 
 
 def lambda_handler(event, context):
-    start_year = 1800
-    end_year = 1950
-    books = search_books(start_year, end_year)
-    store_books_in_db(books)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Books stored in RDS database.')
-    }
+    print('event is ', event)
+    for batch in event['book']:
+        subject = batch['subject']
+        start_index = batch['start_index']
+
+        books = search_books(subject, start_index)
+        store_books_in_db(books)
+
+    db.close()
 
 
 
